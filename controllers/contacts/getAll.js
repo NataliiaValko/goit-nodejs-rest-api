ef452d85a82601d89b91255c4150723dc028fbd6
NataliiaValko <75252511+NataliiaValko@users.noreply.github.com>
2021-11-15 01:42:36 +0200

    adds filter by favorite

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,12 +3,12 @@ const { sendSuccessToRes } = require('../../helpers')
 
 const getAll = async (req, res) => {
   const { _id } = req.user
-  const { page, limit } = req.query
+  const { page, limit, favorite } = req.query
 
   if (page && limit) {
     const skip = (page - 1) * limit
     const result = await Contact.find(
-      { owner: _id },
+      { owner: _id, favorite },
       '_id name email phone favorite owner',
       { skip, limit: +limit }
     ).populate('owner', '_id email')
@@ -18,7 +18,7 @@ const getAll = async (req, res) => {
   }
 
   const result = await Contact.find(
-    { owner: _id },
+    { owner: _id, favorite },
     '_id name email phone favorite owner'
   ).populate('owner', '_id email')
 
