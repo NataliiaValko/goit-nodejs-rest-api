c0549530bd1276cea1397160e5765a8a71b1155b
NataliiaValko <75252511+NataliiaValko@users.noreply.github.com>
2021-11-14 23:35:55 +0200

    adds a parameter check

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,13 +2,24 @@ const { Contact } = require('../../models')
 const { sendSuccessToRes } = require('../../helpers')
 
 const getAll = async (req, res) => {
-  const { page, limit } = req.query
   const { _id } = req.user
-  const skip = (page - 1) * limit
+  const { page, limit } = req.query
+
+  if (page && limit) {
+    const skip = (page - 1) * limit
+    const result = await Contact.find(
+      { owner: _id },
+      '_id name email phone favorite owner',
+      { skip, limit: +limit }
+    ).populate('owner', '_id email')
+
+    sendSuccessToRes(res, { result })
+    return
+  }
+
   const result = await Contact.find(
     { owner: _id },
-    '_id name email phone favorite owner',
-    { skip, limit: +limit }
+    '_id name email phone favorite owner'
   ).populate('owner', '_id email')
 
   sendSuccessToRes(res, { result })
